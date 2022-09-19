ae9bc0094ab2011098ecef28f410e9e33bbd6550
umutcangider <umtcngdr@gmail.com>
2022-09-19 23:34:04 +0300

    SayilariBuyuktenKucugeSiralama added

---
+++ src/SayilariBuyuktenKucugeSiralama/SayilariBuyuktenKucugeSiralama.java
@@ -0,0 +1,46 @@
+package SayilariBuyuktenKucugeSiralama;
+
+import java.util.Scanner;
+
+public class SayilariBuyuktenKucugeSiralama {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("1. Sayı : ");
+        a = input.nextInt();
+
+        System.out.print("2. Sayı : ");
+        b = input.nextInt();
+
+        System.out.print("3. Sayı : ");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+                System.out.println(a + " > " + b + " > " + c);
+            } else {
+                System.out.println("a > c > b");
+                System.out.println(a + " > " + c + " > " + b);
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a > c");
+                System.out.println(b + " > " + a + " > " + c);
+            } else {
+                System.out.println("b > c > a");
+                System.out.println(b + " > " + c + " > " + a);
+            }
+        } else {
+            if (a > b) {
+                System.out.println("c > a > b");
+                System.out.println(c + " > " + a + " > " + b);
+            } else {
+                System.out.println("c > b > a");
+                System.out.println(c + " > " + b + " > " + a);
+            }
+        }
+
+    }
+}
