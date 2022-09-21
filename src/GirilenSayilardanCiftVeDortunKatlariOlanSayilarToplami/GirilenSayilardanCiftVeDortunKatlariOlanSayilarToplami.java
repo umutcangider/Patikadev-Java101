b098b473eacc132ae2a9ef70012afd6eb71f0958
umutcangider <umtcngdr@gmail.com>
2022-09-21 17:30:40 +0300

    GirilenSayilardanCiftVeDortunKatlariOlanSayilarToplami added

---
+++ src/GirilenSayilardanCiftVeDortunKatlariOlanSayilarToplami/GirilenSayilardanCiftVeDortunKatlariOlanSayilarToplami.java
@@ -0,0 +1,21 @@
+package GirilenSayilardanCiftVeDortunKatlariOlanSayilarToplami;
+
+import java.util.Scanner;
+
+public class GirilenSayilardanCiftVeDortunKatlariOlanSayilarToplami {
+    public static void main(String[] args) {
+        int number;
+        int total = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz: ");
+            number = input.nextInt();
+            if(number % 2 == 0 && number % 4 == 0) {
+                total += number;
+            }
+        } while(number % 2 == 0);
+        System.out.println("Toplam : " + total);
+    }
+}
