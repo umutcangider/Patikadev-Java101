ca141ce6452a88b947e3bdf2690f4423e6d62e03
umutcangider <umtcngdr@gmail.com>
2022-09-24 14:19:20 +0300

    MaasHesaplayici added

---
+++ src/MaasHesaplayici/Main.java
@@ -0,0 +1,11 @@
+package MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Kemal",2000,45,1985);
+        Employee employee2 = new Employee("Ahmet",10000,40,2020);
+        System.out.println(employee);
+        System.out.println("----------------------------------------");
+        System.out.println(employee2);
+    }
+}
